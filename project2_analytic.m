029810b998016c60d07991d32cab854ba5a44f8e
TysonWarner <tysonnwarner@gmail.com>
2023-10-31 15:48:11 -0400

    no1 partb

--- project2_analytic.m
+++ project2_analytic.m
@@ -1,5 +1,7 @@
 clear; clc; close all;
+format short
 
+%% Calculating r
 rE = 6378.137;
 
 a1 = 13000; e1 = 0.3;
@@ -17,35 +19,44 @@ A = 1 - 1/e1^2 - (alpha/gamma)^2;
 B = 2*p1/e1^2 - 2*alpha*beta/gamma^2;
 C = -((p1/e1)^2 + (beta/gamma)^2);
 
-r_same_1 = (-B+sqrt(B^2 - 4*A*C))/(2*A);
-r_same_2 = (-B-sqrt(B^2 - 4*A*C))/(2*A);
-r_a = a2 * (1 + e2);
+r = (-B+sqrt(B^2 - 4*A*C))/(2*A);
+% r = (-B-sqrt(B^2 - 4*A*C))/(2*A);
+
+
+SIN = (alpha*r+beta)/(gamma*r)
+COS = (p2/p1 + e2*SIN*sin(dw) - 1)/(e2*cos(dw) - (p2*e1)/p1)
+asind(SIN)
+acosd(COS)
+
+%% Calculating position and velocity vectors
+r_orbital = r*[1 0 0]';
+
+syms theta I Omega omega f
+A = R3(theta+f)*R1(I)*R3(Omega);
+r_eci = simplify(inv(A)*r_orbital);
+r_eci = subs(r_eci,I,deg2rad(20));
+r_eci = subs(r_eci,Omega,deg2rad(30));
 
 
 
 
-f = deg2rad(0:.1:360);
-r1 = zeros(size(f)); r2 = r1;
 
-for i=1:size(f,2)
-    r1(i) = p1 / (1 + e1 * cos(f(i)));
-    r2(i) = p2 / (1 + e2 * cos(f(i)-dw));
+
+
+
+
+
+
+
+function R1 = R1(angle)
+    R1 = [ 1 0 0
+        0 cos(angle) sin(angle)
+        0 -sin(angle) cos(angle)];
 end
-
-plot(f,r1); hold on;
-plot(f,r2); hold on;
-yline([r_same_1 r_same_2])
-
-
-
-
-
-
-
-
-
-
-
-
+function R3 = R3(angle)
+    R3 = [ cos(angle) sin(angle) 0
+        -sin(angle) cos(angle) 0
+        0 0 1];
+end
 
 
